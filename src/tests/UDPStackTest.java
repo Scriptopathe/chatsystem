05abdf2bc33dbbeea9617e52536f7771541a7cbf
Mme Grabot Laura <grabot@geitp103-06.insa-toulouse.fr>
2015-10-20 16:44:11 +0200

    Did something.

--- src/tests/UDPStackTest.java
+++ src/tests/UDPStackTest.java
@@ -1,6 +1,10 @@
 package tests;
 import java.io.IOException;
 import java.net.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import chatsystem.*;
 
 public class UDPStackTest 
@@ -22,24 +26,43 @@ public class UDPStackTest
 		
 		System.out.print(blbl.get("name"));*/
 		
-		// Crée un receiver TCP
-		UDPReceiver rcv = new UDPReceiver(8045);
-		rcv.addListener(new UDPReceiverListener() {
+		int test = 0;
+		if(test == 0)
+		{
+			// Crée un receiver TCP
+			UDPReceiver rcv = new UDPReceiver(8045);
+			rcv.addListener(new UDPReceiverListener() {
+				
+				@Override
+				public void onMessageReceived(InetAddress source, String message) {
+					// TODO Auto-generated method stub
+					System.out.println("Source : " + source + " | Message : " + message);
+				}
+			});
+			// Lance le receiver TCP
+			rcv.start();
 			
-			@Override
-			public void onMessageReceived(InetAddress source, String message) {
-				// TODO Auto-generated method stub
-				System.out.println("Source : " + source + " | Message : " + message);
-			}
-		});
-		// Lance le receiver TCP
-		rcv.start();
-		
-		// Envoie un message au receiver.
-		UDPSender snd = new UDPSender(8045);
-		snd.sendMessage(Inet4Address.getByName("localhost"), new HelloMessage("patate", false));
-		snd.sendMessage(Inet4Address.getByName("localhost"), new HelloMessage("patate", false));
-		snd.sendMessage(Inet4Address.getByName("localhost"), new HelloMessage("patate", false));
+			// Envoie un message au receiver.
+			UDPSender snd = new UDPSender(8045);
+			snd.sendMessage(Inet4Address.getByName("localhost"), new HelloMessage("patate", false));
+			snd.sendMessage(Inet4Address.getByName("10.1.5.85"), new HelloMessage("patate", false));
+		}
+		else
+		{
+			// Crée un receiver TCP
+			UDPReceiver rcv = new UDPReceiver(8045);
+			rcv.addListener(new UDPReceiverListener() {
+				
+				@Override
+				public void onMessageReceived(InetAddress source, String message) {
+					// TODO Auto-generated method stub
+					System.out.println("Source : " + source + " | Message : " + message);
+				}
+			});
+			// Lance le receiver TCP
+			rcv.start();
+			System.out.println("Server running...");
+		}
 		
 		
 		
