56bfef0be1bd53190cddeec71e8a585c8231455d
Josué Alvarez <j_alvare@etud.insa-toulouse.fr>
2015-11-15 19:53:46 +0100

    Ajout de la fonctionnalité de progression.

--- src/chatsystem/network/TCPReceiver.java
+++ src/chatsystem/network/TCPReceiver.java
@@ -5,6 +5,12 @@ import java.util.*;
 import java.io.*;
 import java.net.*;
 
+/**
+ * TCPReceiver a pour tâche de recevoir des données d'un socket pour
+ * les transférer dans un flux de sortie.
+ * @author scriptopathe
+ *
+ */
 public class TCPReceiver extends Thread{
 	
 	private Socket socket;
@@ -12,6 +18,12 @@ public class TCPReceiver extends Thread{
 	private List<TCPReceiverListener> listeners;
 	
 	
+	/**
+	 * Crée une nouvelle instance de TCPReceiver à partir d'un socket et d'un flux
+	 * de sortie.
+	 * @param socket le socket sur lequel lire les données.
+	 * @param stream le stream dans lequel écrire les données lues sur le socket.
+	 */
 	public TCPReceiver(Socket socket, OutputStream stream)
 	{
 		this.socket = socket;
@@ -26,12 +38,24 @@ public class TCPReceiver extends Thread{
 			DataInputStream in = new DataInputStream(this.socket.getInputStream());
 			DataOutputStream out = new DataOutputStream(this.stream);
 			byte[] buffer = new byte[1024];
+			int totalBytes = in.available();
+			int bytesRead = 0;
+			int it = 0;
 			int size = 0;
 			while(size != -1)
 			{
 				size = in.read(buffer);
 				if(size != -1)
+				{
 					out.write(buffer, 0, size);
+					
+					// Mise à jour des progrès de téléchargement.
+					it++;
+					bytesRead += size;
+					if(it % 10 == 0)
+						notifyProgress(this.socket.getInetAddress(), 100*bytesRead/totalBytes);
+				}
+				
 			}
 			
 			notifyEnd(this.socket.getInetAddress());
@@ -63,14 +87,16 @@ public class TCPReceiver extends Thread{
 	}
 	
 	
-	
+	/**
+	 * Ajoute un listener aux évènements de TCPReceiver.
+	 */
 	public void addListeners(TCPReceiverListener t)
 	{
 		this.listeners.add(t);
 	}
 
 
-	public void notifyProgress(InetAddress source, int progress) {
+	private void notifyProgress(InetAddress source, int progress) {
 		
 		for(TCPReceiverListener l : this.listeners)
 		{
@@ -79,7 +105,7 @@ public class TCPReceiver extends Thread{
 	}
 	
 	
-	public void notifyEnd(InetAddress source) {
+	private void notifyEnd(InetAddress source) {
 		
 		for(TCPReceiverListener l : this.listeners)
 		{
