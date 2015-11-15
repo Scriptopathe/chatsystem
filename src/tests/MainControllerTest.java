f863f74e1500bc77c31923c071fe8214d80298ae
Josué Alvarez <j_alvare@etud.insa-toulouse.fr>
2015-11-15 19:54:12 +0100

    Mise à jour du test du main controler

--- src/tests/MainControllerTest.java
+++ src/tests/MainControllerTest.java
@@ -3,18 +3,59 @@ package tests;
 import java.util.Scanner;
 
 import chatsystem.controler.MainController;
+import chatsystem.controler.MainControllerListener;
 import chatsystem.model.User;
-
+import chatsystem.gui.*;
 public class MainControllerTest {
 	public static void main(String[] args)
 	{
-		MainController ctrl1 = new MainController();
-		ctrl1.connect("CTRL 1");
+		ChatGUI g = new ChatGUI();
 		
+		MainController ctrl1 = new MainController();
+		ctrl1.connect("Benard");
+		ctrl1.addListener(new MainControllerListener() {
+			
+			@Override
+			public void OnUserDisconnected(User usr) {
+				System.out.println("User " + usr + " disconnected");
+				
+			}
+			
+			@Override
+			public void OnUserConnected(User usr) {
+				// TODO Auto-generated method stub
+				System.out.println("User " + usr + " connected");
+				
+			}
+			
+			@Override
+			public void OnMessageReceived(User usr, String textMessage) {
+				// TODO Auto-generated method stub
+				System.out.println("User " + usr + " says : " + textMessage);
+			}
+			
+			@Override
+			public void OnLog(String text, boolean isError) {
+				// TODO Auto-generated method stub
+				System.out.println("[CtrlLog] " + text);
+			}
+			
+			@Override
+			public void OnFileTransferEnded(User usr, String filename) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void OnFileRequest(User usr, String filename) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		while(true)
 		{
 			Scanner s = new Scanner(System.in);
-
+			
 			User usr = ctrl1.getUserList().getUserByNickname("mikael");
 			if(usr != null)
 			{
